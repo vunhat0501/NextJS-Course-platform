3b8d4d3f569c8ae2dca3f1565604ea0cbca4dab4
Nhật Vũ <vunhat4869@gmail.com>
2025-05-14 13:32:29 +0700

    remove canary

--- next.config.ts
+++ next.config.ts
@@ -2,10 +2,7 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
     /* config options here */
-    experimental: {
-        dynamicIO: true,
-        authInterrupts: true,
-    },
+    experimental: {},
 };
 
 export default nextConfig;
