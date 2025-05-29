db6810c0f37eeaf5c5413e2f7f98e6f22baee085
dangquan18 <dangq2359@gmail.com>
2025-05-29 23:41:42 +0700

    checkout-form

--- src/services/stripe/components/StripeCheckoutForm.tsx
+++ src/services/stripe/components/StripeCheckoutForm.tsx
@@ -1,36 +1,40 @@
-"use client"
+'use client';
 
 import {
-  EmbeddedCheckoutProvider,
-  EmbeddedCheckout,
-} from "@stripe/react-stripe-js"
-import { stripeClientPromise } from "../stripeClient"
-import { getClientSessionSecret } from "../actions/stripe"
+    EmbeddedCheckoutProvider,
+    EmbeddedCheckout,
+} from '@stripe/react-stripe-js';
+import { stripeClientPromise } from '../stripeClient';
+import { getClientSessionSecret } from '../actions/stripe';
 
 export function StripeCheckoutForm({
-  product,
-  user,
+    product,
+    user,
 }: {
-  product: {
-    priceInDollars: number
-    name: string
-    id: string
-    imageUrl: string
-    description: string
-  }
-  user: {
-    email: string
-    id: string
-  }
+    product: {
+        priceInDollars: number;
+        name: string;
+        id: string;
+        image_url: string;
+        description: string;
+    };
+    user: {
+        email: string;
+        id: string;
+    };
 }) {
-  return (
-    <EmbeddedCheckoutProvider
-      stripe={stripeClientPromise}
-      options={{
-        fetchClientSecret: getClientSessionSecret.bind(null, product, user),
-      }}
-    >
-      <EmbeddedCheckout />
-    </EmbeddedCheckoutProvider>
-  )
+    return (
+        <EmbeddedCheckoutProvider
+            stripe={stripeClientPromise}
+            options={{
+                fetchClientSecret: getClientSessionSecret.bind(
+                    null,
+                    product,
+                    user,
+                ),
+            }}
+        >
+            <EmbeddedCheckout />
+        </EmbeddedCheckoutProvider>
+    );
 }
