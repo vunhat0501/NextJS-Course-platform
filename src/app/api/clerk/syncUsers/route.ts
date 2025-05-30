885695fe3190f3c28b1bef6e74888242d9fad2c9
Nhật Vũ <vunhat4869@gmail.com>
2025-05-30 23:03:16 +0700

    sync user page

--- src/app/api/clerk/syncUsers/route.ts
+++ src/app/api/clerk/syncUsers/route.ts
@@ -3,10 +3,36 @@ import { syncClerkUserMetaData } from '@/services/clerk';
 import { currentUser } from '@clerk/nextjs/server';
 import { NextResponse } from 'next/server';
 
-export async function GET(request: Request) {
-    const user = await currentUser();
+// export async function GET(request: Request) {
+//     const user = await currentUser();
 
+//     if (user == null) return new Response('User not found', { status: 500 });
+//     if (user.fullName == null) {
+//         return new Response('User name missing', { status: 500 });
+//     }
+//     if (user.primaryEmailAddress?.emailAddress == null) {
+//         return new Response('User email missing', { status: 500 });
+//     }
+
+//     const dbUser = await insertUser({
+//         clerkUserId: user.id,
+//         name: user.fullName,
+//         email: user.primaryEmailAddress.emailAddress,
+//         imageUrl: user.imageUrl,
+//         role: user.publicMetadata.role ?? 'user',
+//     });
+
+//     await syncClerkUserMetaData(dbUser);
+
+//     await new Promise((res) => setTimeout(res, 100));
+
+//     return NextResponse.redirect(request.headers.get('referer') ?? '/');
+// }
+
+export async function GET() {
+    const user = await currentUser();
     if (user == null) return new Response('User not found', { status: 500 });
+
     if (user.fullName == null) {
         return new Response('User name missing', { status: 500 });
     }
@@ -26,5 +52,5 @@ export async function GET(request: Request) {
 
     await new Promise((res) => setTimeout(res, 100));
 
-    return NextResponse.redirect(request.headers.get('referer') ?? '/');
+    return NextResponse.json({ success: true });
 }
